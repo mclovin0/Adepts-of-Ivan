231552d663904eb0243764d9f7fda8e55b94ac10
Ruslan Matus <ryslanx666@ukr.net>
2019-03-25 20:52:40 +0300

    refactoring of function

--- emotion_classifier.py
+++ emotion_classifier.py
@@ -1,22 +1,24 @@
 import numpy as np
 from keras.models import load_model
-def preprocess_input(x, v2=True):
-    x = x.astype('float32')
-    x = x / 255.0
-    if v2:
-        x = x - 0.5
-        x = x * 2.0
-    return x
-
-def get_emotion(image):
-    emotion_labels = {0:'angry',1:'disgust',2:'fear',3:'happy',4:'sad',5:'surprise',6:'neutral'}
-    emotion_model_path = './models/emotion_model.hdf5'
-    emotion_classifier = load_model(emotion_model_path)
-    image = preprocess_input(image, True)
-    image = np.expand_dims(image, 0)
-    image = np.expand_dims(image, -1)
-    emotion_prediction = emotion_classifier.predict(image)
-    emotion_probability_arg = np.argmax(emotion_prediction)
-    emotion_text = emotion_labels[emotion_probability_arg]
-    return emotion_text
-
+class EmotionClassifier:
+    def __init__(self, image):
+        self.emotion_labels = {0:'angry',1:'disgust',2:'fear',3:'happy',4:'sad',5:'surprise',6:'neutral'}
+        self.emotion_classifier = load_model('./models/emotion_model.hdf5')
+        self.image = image
+        self.emotion_prediction = ''
+        self.emotion_probability_arg = ''
+        self.emotion_text = ''
+    def preprocess_input(self, v2=True):
+        self.image = self.image.astype('float32')
+        self.image = self.image / 255.0
+        if v2:
+            self.image = self.image - 0.5
+            self.image = self.image * 2.0
+        self.image = np.expand_dims(self.image, 0)
+        self.image = np.expand_dims(self.image, -1)
+        return self.image
+    def get_emotion(self):
+        self.emotion_prediction = self.emotion_classifier.predict(self.preprocess_input(self.image))
+        self.emotion_probability_arg = np.argmax(self.emotion_prediction)
+        self.emotion_text = self.emotion_labels[self.emotion_probability_arg]
+        return self.emotion_text
