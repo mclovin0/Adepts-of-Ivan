ced751df10b14f287b128be36bd191a97595ed1e
Ruslan Matus <ryslanx666@ukr.net>
2019-04-14 13:59:11 +0300

    new function

--- emotion_classifier.py
+++ emotion_classifier.py
@@ -1,24 +1,22 @@
 import numpy as np
-from keras.models import load_model
+import cv2
 class EmotionClassifier:
-    def __init__(self, image):
-        self.emotion_labels = {0:'angry',1:'disgust',2:'fear',3:'happy',4:'sad',5:'surprise',6:'neutral'}
-        self.emotion_classifier = load_model('./models/emotion_model.hdf5')
-        self.image = image
-        self.emotion_prediction = ''
-        self.emotion_probability_arg = ''
-        self.emotion_text = ''
-    def preprocess_input(self, v2=True):
-        self.image = self.image.astype('float32')
-        self.image = self.image / 255.0
-        if v2:
-            self.image = self.image - 0.5
-            self.image = self.image * 2.0
-        self.image = np.expand_dims(self.image, 0)
-        self.image = np.expand_dims(self.image, -1)
-        return self.image
-    def get_emotion(self):
-        self.emotion_prediction = self.emotion_classifier.predict(self.preprocess_input(self.image))
-        self.emotion_probability_arg = np.argmax(self.emotion_prediction)
-        self.emotion_text = self.emotion_labels[self.emotion_probability_arg]
-        return self.emotion_text
+    def __init__(self):
+        self.face_emotion = cv2.face.FisherFaceRecognizer_create()
+        self.path = 'models/emotion_classifier_model.xml'
+        self.delay = 0
+        self.init = True
+        self.emotions = ["afraid", "angry", "disgusted", "happy", "neutral", "sad", "surprised"]
+    def emotion_classify(self):
+        image = cv2.imread("sample.jpg", 1)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.resize(image, (350, 350))
+        self.face_emotion.read(self.path)
+        if self.init or self.delay == 0:
+            self.init = False
+            emotion_prediction = self.face_emotion.predict(image)
+            print(self.emotions[emotion_prediction[0]])
+
+
+eml = EmotionClassifier()
+eml.emotion_classify()
